adda87d514fcc95dbba5f480c66693cd5e9964c9
James Henderson <james@jarohen.dev>
2022-05-11 11:45:16 +0100

    bump XTDB HTTP deps, rename from Crux

--- xtdb-http-server-deps/project.clj
+++ xtdb-http-server-deps/project.clj
@@ -1,11 +1,12 @@
-(defproject pro.juxt.clojars-mirrors.crux/crux-http-server-deps "0.0.2"
-  :description "Crux's http-server deps mirrored from Clojars. See individual projects for licenses."
+(defproject pro.juxt.clojars-mirrors.xtdb/xtdb-http-server-deps "0.0.2"
+  :description "XTDB's http-server deps mirrored from Clojars. See individual projects for licenses."
   :url "https://github.com/juxt/clojars-mirrors"
   :licenses [{:name "Eclipse Public License"
               :url "http://www.eclipse.org/legal/epl-v10.html"}]
   :scm {:dir ".."}
 
-  :dependencies [^:inline-dep [ring/ring-core "1.9.2"
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 ^:inline-dep [ring/ring-core "1.9.4"
                                :exclusions [commons-fileupload
                                             commons-io
                                             commons-codec
@@ -20,37 +21,37 @@
                  ^:inline-dep [ring-cors "0.1.13"]
                  ^:inline-dep [metosin/muuntaja "0.6.8"
                                :exclusions [com.cognitect/transit-clj]]
-                 ^:inline-dep [metosin/jsonista "0.3.1"
+                 ^:inline-dep [metosin/jsonista "0.3.5"
                                :exclusions [com.fasterxml.jackson.datatype/jackson-datatype-jsr310
                                             com.fasterxml.jackson.core/jackson-core
                                             com.fasterxml.jackson.core/jackson-databind]]
-                 ^:inline-dep [metosin/reitit-core "0.5.12"]
-                 ^:inline-dep [metosin/reitit-swagger "0.5.12"]
-                 ^:inline-dep [metosin/reitit-middleware "0.5.12"
+                 ^:inline-dep [metosin/reitit-core "0.5.15"]
+                 ^:inline-dep [metosin/reitit-swagger "0.5.15"]
+                 ^:inline-dep [metosin/reitit-middleware "0.5.15"
                                :exclusions [org.clojure/spec.alpha
                                             org.clojure/core.rrb-vector]]
-                 ^:inline-dep [metosin/reitit-ring "0.5.12"]
-                 ^:inline-dep [metosin/reitit-spec "0.5.12"]
+                 ^:inline-dep [metosin/reitit-ring "0.5.15"]
+                 ^:inline-dep [metosin/reitit-spec "0.5.15"]
 
-                 [org.eclipse.jetty/jetty-alpn-openjdk8-server "9.4.36.v20210114"]
 
                  ;; transitive deps from the above
-                 [org.eclipse.jetty.http2/http2-server "9.4.36.v20210114"]
-                 [org.eclipse.jetty.websocket/websocket-server "9.4.36.v20210114"]
-                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.36.v20210114"]
-                 [org.eclipse.jetty/jetty-alpn-server "9.4.36.v20210114"]
-                 [org.eclipse.jetty/jetty-server "9.4.36.v20210114"]
+                 [org.eclipse.jetty.http2/http2-server "9.4.43.v20210629"]
+                 [org.eclipse.jetty.websocket/websocket-server "9.4.43.v20210629"]
+                 [org.eclipse.jetty.websocket/websocket-servlet "9.4.43.v20210629"]
+                 [org.eclipse.jetty/jetty-alpn-server "9.4.43.v20210629"]
+                 [org.eclipse.jetty/jetty-alpn-openjdk8-server "9.4.43.v20210629"]
+                 [org.eclipse.jetty/jetty-server "9.4.43.v20210629"]
                  [com.cognitect/transit-clj "1.0.324"]
-                 [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.12.1"]
-                 [com.fasterxml.jackson.core/jackson-core "2.12.1"]
-                 [com.fasterxml.jackson.core/jackson-databind "2.12.1"]
-                 [org.clojure/core.rrb-vector "0.0.14"]
-                 [org.clojure/spec.alpha "0.2.187"]
+                 [com.fasterxml.jackson.datatype/jackson-datatype-jsr310 "2.12.5"]
+                 [com.fasterxml.jackson.core/jackson-core "2.12.5"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.12.5"]
+                 [org.clojure/core.rrb-vector "0.1.2"]
+                 [org.clojure/spec.alpha "0.2.194"]
                  [commons-fileupload "1.4"]
-                 [commons-io "2.6"]
+                 [commons-io "2.11.0"]
                  [commons-codec "1.15"]
                  [crypto-equality "1.0.0"]
-                 [crypto-random "1.2.0"]]
+                 [crypto-random "1.2.1"]]
 
   :exclusions [org.clojure/clojure]
 
