d5317195db7fa85acdef2b5b8856b0605c3149f0
James Henderson <james@jarohen.dev>
2024-06-19 19:15:08 +0100

    bump Nippy. also remove the `-` from the prefix because benedekfazekas/mranderson#73

--- com.taoensso/nippy/project.clj
+++ com.taoensso/nippy/project.clj
@@ -1,30 +1,33 @@
-(defproject pro.juxt.clojars-mirrors.com.taoensso/nippy "3.1.1-2"
+(defproject pro.juxt.clojars-mirrors.com.taoensso/nippy "3.4.2-1"
   :description "High-performance serialization library for Clojure (mirrored from Clojars by JUXT)"
   :url "https://github.com/ptaoussanis/nippy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:dir "../.."}
 
-  :dependencies [^:inline-dep
-                 [com.taoensso/nippy "3.1.1" :exclusions [org.clojure/tools.reader
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 ^:inline-dep
+                 [com.taoensso/nippy "3.4.2" :exclusions [org.clojure/tools.reader
                                                           org.iq80.snappy/snappy
                                                           org.lz4/lz4-java
-                                                          org.tukaani/xz]]
+                                                          org.tukaani/xz
+                                                          io.airlift/aircompressor]]
 
                  ^:inline-dep
-                 [com.taoensso/encore "3.21.0" :exclusions [org.clojure/tools.reader
-                                                            org.iq80.snappy/snappy
-                                                            org.lz4/lz4-java
-                                                            org.tukaani/xz]]
+                 [com.taoensso/encore "3.112.0" :exclusions [org.clojure/tools.reader
+                                                             org.iq80.snappy/snappy
+                                                             org.lz4/lz4-java
+                                                             org.tukaani/xz]]
 
                  [org.clojure/tools.reader "1.3.4"]
                  [org.iq80.snappy/snappy "0.4"]
                  [org.lz4/lz4-java "1.7.1"]
-                 [org.tukaani/xz "1.8"]]
+                 [org.tukaani/xz "1.8"]
+                 [io.airlift/aircompressor "0.27"]]
 
   :plugins [[io.github.jarohen/mranderson "0.5.4-SNAPSHOT"]]
 
-  :mranderson {:project-prefix "juxt.clojars-mirrors"}
+  :mranderson {:project-prefix "juxt.clojars.mirrors"}
 
   :jar-exclusions [#".cljx$"]
 
